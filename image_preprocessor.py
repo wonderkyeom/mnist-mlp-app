57796ad737bbe70688f561c5a7fc3a6382c2639b
wonderkyeom <wonderkyum21@naver.com>
2021-03-27 13:00:32 +0900

    0327

---
+++ image_preprocessor.py
@@ -0,0 +1,8 @@
+from PIL import Image, ImageOps
+
+def preprocess(src, width, height):
+   gray_image = src.convert('L')
+   resized_image = gray_image.resize((width, height))
+   dst = ImageOps.invert(resized_image)
+   return dst
+
